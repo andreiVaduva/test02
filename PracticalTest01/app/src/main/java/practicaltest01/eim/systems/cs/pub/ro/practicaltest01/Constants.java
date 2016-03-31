be578fe2383051679265613bf7a3c1bd0cfe9d69
andreiVaduva <vaduva.andrei.iustin@gmail.com>
2016-03-31 16:34:14 +0300

    finished application

---
+++ PracticalTest01/app/src/main/java/practicaltest01/eim/systems/cs/pub/ro/practicaltest01/Constants.java
@@ -0,0 +1,16 @@
+package practicaltest01.eim.systems.cs.pub.ro.practicaltest01;
+
+public interface Constants {
+
+    final public static String[] actionTypes = {
+            "ro.pub.cs.systems.eim.practicaltest01.actionType1",
+            "ro.pub.cs.systems.eim.practicaltest01.actionType2",
+            "ro.pub.cs.systems.eim.practicaltest01.actionType3"
+    };
+
+    final public static int NUMBER_OF_CLICKS_THRESHOLD = 5;
+
+    final public static int SERVICE_STARTED = 1;
+    final public static int SERVICE_STOPPED = 2;
+
+}
--- PracticalTest01/app/src/main/java/practicaltest01/eim/systems/cs/pub/ro/practicaltest01/PracticalTest01MainActivity.java
+++ PracticalTest01/app/src/main/java/practicaltest01/eim/systems/cs/pub/ro/practicaltest01/PracticalTest01MainActivity.java
@@ -1,12 +1,23 @@
 package practicaltest01.eim.systems.cs.pub.ro.practicaltest01;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class PracticalTest01MainActivity extends AppCompatActivity {
+
+    public final static int SECONDARY_ACTIVITY_REQUEST_CODE = 1;
+
+    private IntentFilter intentFilter = new IntentFilter();
 
     private Button navigateToSecondaryActivityButton = null;
     private Button leftButton = null;
@@ -15,24 +26,52 @@ public class PracticalTest01MainActivity extends AppCompatActivity {
     private EditText leftEditText = null;
     private EditText rightEditText = null;
 
+    private int serviceStatus = Constants.SERVICE_STOPPED;
+
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
     private class ButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+
+            int leftNumberOfClicks = Integer.parseInt(leftEditText.getText().toString());
+            int rightNumberOfClicks = Integer.parseInt(rightEditText.getText().toString());
+
             switch (view.getId()) {
                 case R.id.navigate_to_secondary_activity_button:
+                    Intent intent = new Intent(getApplicationContext(), PracticalTest01SecondaryActivity.class);
+                    int numberOfClicks = Integer.parseInt(leftEditText.getText().toString()) +
+                                         Integer.parseInt(leftEditText.getText().toString());
+                    intent.putExtra("numberOfClicks", numberOfClicks);
+                    startActivityForResult(intent, SECONDARY_ACTIVITY_REQUEST_CODE);
                     break;
                 case R.id.left_button:
-                    int leftNumberOfClicks = Integer.parseInt(leftEditText.getText().toString());
+                    leftNumberOfClicks = Integer.parseInt(leftEditText.getText().toString());
                     leftNumberOfClicks++;
                     leftEditText.setText(String.valueOf(leftNumberOfClicks));
                     break;
                 case R.id.right_button:
-                    int rightNumberOfClicks = Integer.parseInt(rightEditText.getText().toString());
+                    rightNumberOfClicks = Integer.parseInt(rightEditText.getText().toString());
                     rightNumberOfClicks++;
                     rightEditText.setText(String.valueOf(rightNumberOfClicks));
                     break;
             }
+
+            if (leftNumberOfClicks + rightNumberOfClicks > Constants.NUMBER_OF_CLICKS_THRESHOLD &&
+                    serviceStatus == Constants.SERVICE_STOPPED) {
+                Intent intent = new Intent(getApplicationContext(), PracticalTest01Service.class);
+                intent.putExtra("firstNumber", leftNumberOfClicks);
+                intent.putExtra("secondNumber", rightNumberOfClicks);
+                getApplicationContext().startService(intent);
+                serviceStatus = Constants.SERVICE_STARTED;
+            }
+        }
+    }
+
+    private MessageBroadcastReceiver messageBroadcastReceiver = new MessageBroadcastReceiver();
+    private class MessageBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("[Message]", intent.getStringExtra("message"));
         }
     }
 
@@ -53,6 +92,36 @@ public class PracticalTest01MainActivity extends AppCompatActivity {
 
         navigateToSecondaryActivityButton = (Button)findViewById(R.id.navigate_to_secondary_activity_button);
         navigateToSecondaryActivityButton.setOnClickListener(buttonClickListener);
+
+        for(int index = 0; index < Constants.actionTypes.length; ++index) {
+            intentFilter.addAction(Constants.actionTypes[index]);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(messageBroadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(messageBroadcastReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Intent intent = new Intent(this, PracticalTest01Service.class);
+        stopService(intent);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SECONDARY_ACTIVITY_REQUEST_CODE) {
+            Toast.makeText(this, "The activity returned with result " + resultCode, Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
